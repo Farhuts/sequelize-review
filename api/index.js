b17b0ea3054a045e37039121f8400c163b4bb2b8
Verquantumica <veronicaburnham@gmail.com>
2019-01-29 10:40:46 -0500

    empty for student demo

--- api/index.js
+++ api/index.js
@@ -2,53 +2,17 @@ const router = require('express').Router()
 const {User, Tweet} = require('./db/models'); 
 
 //write a route to the path /:userId where you return all tweets from that user
-router.get('/:userId', async (req, res, next) => { 
-    try {
-        const tweets = await Tweet.findAll({ 
-            where: { 
-                tweeterId: req.params.userId
-            }
-        })
-        res.json(tweets); 
-    } catch (error) {
-        console.error(error); 
-    }
-})
+
 
 //write a post route to post a new tweet from a /:userId 
 //make sure to set the association! 
 //send back the newly created tweet 
-router.post('/:userId', async (req, res, next) => { 
-    try {
-        const tweet = await Tweet.create({ //create not build
-            content: req.body.content
-        })
-        const tweeter = await User.findById(req.params.userId); 
-        tweet.setTweeter(tweeter); 
-        res.json(tweet); 
-    } catch (error) {
-        console.error(error); 
-    }
-})
+
 
 //write a route to update a tweet 
 //return the updated tweet 
-router.put(':/userId/tweets/:tweetId', async (req, res, next) => { 
-   try {
-       const update = await Tweet.update(req.body, { 
-           where: { 
-               id: req.params.tweetId
-           }, 
-           returning: true //return the updated columns 
-       })
-       res.json({
-           message: 'Updated successfully', 
-           tweet: update[1][0]
-       })
-   } catch (error) {
-       console.error(error); 
-   }
-})
+
+
 
 
 module.exports = router; 
--- db/models.js
+++ db/models.js
@@ -1,65 +1,20 @@
 const Sequelize = require('sequelize')
 const db = require('./db')
 
-const User = db.define('user', {
     //write  a user model that contains a name, an email, and a password 
     //write validations athat make sure the fields are not empty and inclue a special validation for email
     //write a hook that hashes the password before the instance is created, using the given hash function 
     //write a getter method that causes the email to be returned as a lower case string
-    name: { 
-        type: Sequelize.STRING, 
-        validate : {
-            notEmpty: true
-        } 
-    }, 
-    email: { 
-        type: Sequelize.STRING, 
-        validate : { 
-            isEmail: true, 
-            notEmpty: true
-        },
-        get() { 
-            return this.getDataValue('name').toLowerCase(); 
-        }
-    }, 
-    password: { 
-        type: Sequelize.STRING, 
-        validate : { 
-            notEmpty: true
-        }
-    }, 
-    hooks: {
-        beforeCreate: (user, options) => { 
-            user.password = hash(user.password); 
-        }
-    }
 
-})
 
-const Tweet = db.define('tweet', {
+
     //write a tweet model that contains content, favorites, and retweets
     //make sure the content can't be null and give the other two a default value 
-    content : { 
-        type: Sequelize.TEXT, 
-        allowNull: false
-    }, 
-    favorites: { 
-        type: Sequelize.INTEGER, 
-        defaultValue: 0
-    }, 
-    retweets: { 
-        type: Sequelize.INTEGER, 
-        defaultValue: 0
-    }, 
 
-})
 
 //alt method for hook 
-User.beforeCreate((user, options) => { 
-    let hashedPassword = hash(user.password); 
-    user.password = hashedPassword; 
-})
 
+//use this hashfunction 
 function hash(str) {
     let hash = 5381; 
     let i = str.length;
@@ -72,12 +27,7 @@ function hash(str) {
 
 //set asssociations, using the "as" keyword 
 
-Tweet.belongsTo(User, {as: 'tweeter'}); 
-User.hasMany(Tweets); 
+
 
 
 module.exports = {User, Tweet}
-
-//review hooks 
-//have some tags thing 
-//class method and instance method 
